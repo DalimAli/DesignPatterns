9a6240c58fb06a9e6862c03fca1f27da88c20355
DalimAli <dalim3281@gmail.com>
2024-11-08 19:13:16 +0600

    Facade implemented

---
+++ Structural/Facade.DesignPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿// Usage:
+Facade facade = new Facade();
+facade.PerformOperations();
+
+public class SubsystemA
+{
+    public void OperationA() => Console.WriteLine("Subsystem A operation.");
+}
+
+public class SubsystemB
+{
+    public void OperationB() => Console.WriteLine("Subsystem B operation.");
+}
+
+public class Facade
+{
+    private SubsystemA _subsystemA = new SubsystemA();
+    private SubsystemB _subsystemB = new SubsystemB();
+
+    public void PerformOperations()
+    {
+        _subsystemA.OperationA();
+        _subsystemB.OperationB();
+    }
+}
