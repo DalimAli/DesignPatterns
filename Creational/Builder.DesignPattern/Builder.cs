b21081396bd438ba565354493aaa7b5f8398c9db
DalimAli <dalim3281@gmail.com>
2024-11-08 18:33:32 +0600

    Builder design pattern implemented

---
+++ Creational/Builder.DesignPattern/Builder.cs
@@ -0,0 +1,38 @@
+﻿namespace Builder.DesignPattern;
+
+public class Car
+{
+    public string Engine { get; set; }
+    public string Tires { get; set; }
+    public string Seats { get; set; }
+
+    public override string ToString() => $"Engine: {Engine}, Tires: {Tires}, Seats: {Seats}";
+}
+
+public class CarBuilder
+{
+    private Car car = new Car();
+
+    public CarBuilder SetEngine(string engine)
+    {
+        car.Engine = engine;
+        return this;
+    }
+
+    public CarBuilder SetTires(string tires)
+    {
+        car.Tires = tires;
+        return this;
+    }
+
+    public CarBuilder SetSeats(string seats)
+    {
+        car.Seats = seats;
+        return this;
+    }
+
+    public Car Build()
+    {
+        return car;
+    }
+}
---
+++ Creational/Builder.DesignPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿using Builder.DesignPattern;
+
+Car car = new CarBuilder()
+        .SetTires("Michelin")
+        .SetSeats("Leather")
+        .SetEngine("Engine")
+        .Build();
+
+Console.WriteLine(car);
+Console.ReadKey();
