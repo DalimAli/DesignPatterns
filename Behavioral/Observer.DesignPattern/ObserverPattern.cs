9a00dd780fe72503ee8a8f33b8ca579898a3cffb
DalimAli <dalim3281@gmail.com>
2024-11-08 19:46:49 +0600

    observer implemented

---
+++ Behavioral/Observer.DesignPattern/ObserverPattern.cs
@@ -0,0 +1,37 @@
+﻿namespace Observer.DesignPattern;
+
+public interface IObserver
+{
+    void Update();
+}
+
+public class Subject
+{
+    private List<IObserver> observers = new List<IObserver>();
+
+    public void Attach(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void Notify()
+    {
+        foreach (var observer in observers)
+        {
+            observer.Update();
+        }
+    }
+}
+
+public class ConcreteObserver : IObserver
+{
+    public void Update()
+    {
+        Console.WriteLine("Observer has been notified.");
+    }
+}
---
+++ Behavioral/Observer.DesignPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// Usage:
+using Observer.DesignPattern;
+
+Subject subject = new Subject();
+ConcreteObserver observer = new ConcreteObserver();
+subject.Attach(observer);
+subject.Notify(); // Output: Observer has been notified.
+
