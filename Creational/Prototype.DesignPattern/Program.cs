d88504aebf94f871a2d17ce5b365af6d06850b79
DalimAli <dalim3281@gmail.com>
2024-11-08 18:49:47 +0600

    Prototype design pattern implemented

---
+++ Creational/Prototype.DesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using Prototype.DesignPattern;
+
+ConcretePrototype concretePrototype = new ConcretePrototype()
+{
+    Id = 1,
+    Data = "Proto"
+};
+
+ConcretePrototype clone = (ConcretePrototype) concretePrototype.Clone();
+clone.Id = 2;
+
+Console.WriteLine(concretePrototype);
+Console.WriteLine(clone);
---
+++ Creational/Prototype.DesignPattern/Prototype.cs
@@ -0,0 +1,21 @@
+﻿namespace Prototype.DesignPattern;
+
+public abstract class Prototype
+{
+    public abstract Prototype Clone();
+}
+
+public class ConcretePrototype : Prototype
+{
+    public int Id { get; set; }
+    public string Data { get; set; }
+    public override Prototype Clone()
+    {
+        return (Prototype)this.MemberwiseClone();
+    }
+
+    public override string ToString()
+    {
+        return $"Id {Id}, Data {Data}";
+    }
+}
