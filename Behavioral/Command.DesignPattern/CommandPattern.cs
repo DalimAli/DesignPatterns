d66e7e88f34948ede8df0a868b3ec475caf3b0d9
DalimAli <dalim3281@gmail.com>
2024-11-09 06:40:17 +0600

    command pattern implemented

---
+++ Behavioral/Command.DesignPattern/CommandPattern.cs
@@ -0,0 +1,42 @@
+﻿namespace Command.DesignPattern;
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class Light
+{
+    public void TurnOn() => Console.WriteLine("The light is on.");
+    public void TurnOff() => Console.WriteLine("The light is off.");
+}
+
+public class TurnOnCommand : ICommand
+{
+    private Light _light;
+
+    public TurnOnCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.TurnOn();
+    }
+}
+
+public class TurnOffCommand : ICommand
+{
+    private Light _light;
+
+    public TurnOffCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.TurnOff();
+    }
+}
---
+++ Behavioral/Command.DesignPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿using Command.DesignPattern;
+
+Light light = new Light();
+ICommand turnOnCommand = new TurnOnCommand(light);
+ICommand turnOffCommand = new TurnOffCommand(light);
+
+turnOnCommand.Execute(); // Output: The light is on.
+turnOffCommand.Execute(); // Output: The light is off.
